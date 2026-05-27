bfcad61354dbaba475ddfaeeb689342c747a8527
jesús david barrera montes <jesusdavidbm@ufps.edu.co>
2026-05-27 17:52:45 -0500

    test: violación intencional de arquitectura (dominio depende de infraestructura)

--- src/main/java/com/universidad/sistemapedidos/pedidos/domain/Pedido.java
+++ src/main/java/com/universidad/sistemapedidos/pedidos/domain/Pedido.java
@@ -1,5 +1,7 @@
 package com.universidad.sistemapedidos.pedidos.domain;
 
+import com.universidad.sistemapedidos.pedidos.infrastructure.PedidoRepository;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -9,6 +11,7 @@ import jakarta.persistence.Id;
 
 @Entity
 public class Pedido {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -19,7 +22,8 @@ public class Pedido {
     @Enumerated(EnumType.STRING)
     private EstadoPedido estado;
 
-    public Pedido() {}
+    public Pedido() {
+    }
 
     public Pedido(TipoPedido tipo, double subtotal) {
         this.tipo = tipo;
@@ -27,15 +31,48 @@ public class Pedido {
         this.estado = EstadoPedido.PENDIENTE;
     }
 
+    public void metodoProhibido() {
+        PedidoRepository repo = null; // esto ya es dependencia prohibida
+    }
+
     // Getters y setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public TipoPedido getTipo() { return tipo; }
-    public void setTipo(TipoPedido tipo) { this.tipo = tipo; }
-    public double getSubtotal() { return subtotal; }
-    public void setSubtotal(double subtotal) { this.subtotal = subtotal; }
-    public double getCosto() { return costo; }
-    public void setCosto(double costo) { this.costo = costo; }
-    public EstadoPedido getEstado() { return estado; }
-    public void setEstado(EstadoPedido estado) { this.estado = estado; }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public TipoPedido getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoPedido tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(double costo) {
+        this.costo = costo;
+    }
+
+    public EstadoPedido getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoPedido estado) {
+        this.estado = estado;
+    }
 }
